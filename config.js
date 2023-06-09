70bbaff18860d89fc8b1439298b88876ab26bad6
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-06-09 22:26:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,30 +1,25 @@
-function geoFindMe() {
-    const status = document.querySelector("#status");
-    const mapLink = document.querySelector("#map-link");
-  
-    mapLink.href = "";
-    mapLink.textContent = "";
-  
-    function success(position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      status.textContent = "";
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+const findMyState = () => {
+    const status = document.querySelector('.status');
+
+    const success = (position) => {
+        console.log(position)
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        const geoApiUrl = 'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en'
+
+        fetch(geoApiUrl)
+        .then(res => res.json())
+        .then(data => {
+            status.textContent = data.principalSubdivision
+        })
     }
-  
-    function error() {
-      status.textContent = "Unable to retrieve your location";
+
+    const error = () => {
+            status.textContent = 'Unable to Find'
     }
-  
-    if (!navigator.geolocation) {
-      status.textContent = "Geolocation is not supported by your browser";
-    } else {
-      status.textContent = "Locating…";
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-  }
-  
-  document.querySelector("#find-me").addEventListener("click", geoFindMe);
-  
+
+        navigator.geolocation.getCurrentPosition(success, error);
+}
+
+    document.querySelector('.find-state').addEventListener('click', findMyState);
